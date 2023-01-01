f7c622b704a2ab4bdac4eb1d0a566c3740e0815e
Swoad-11 <toufiq11swoad@gmail.com>
2023-01-01 18:20:01 +0600

    tailwind fixed

---
+++ bank_app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className='bg-black w-full'>
+      <h1 className='text-red-500'>Hello</h1>
+      <p className='bg-red-400'>ashasbhdsdfadfa</p>
+    </div>
+  );
+};
+
+export default App;
